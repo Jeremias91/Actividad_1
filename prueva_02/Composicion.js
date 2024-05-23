e065b05ca9075d3cf026e9939cb7517014fae001
Jeremias91 <126523676+Jeremias91@users.noreply.github.com>
2024-05-23 20:00:40 -0300

    Update Composicion.js

--- prueva_02/Composicion.js
+++ prueva_02/Composicion.js
@@ -1,1 +1,22 @@
 "use strict";
+class Book {
+
+    constructor(nombreLibro){
+        this.nombreLibro = nombreLibro;
+        this.hojas = new Page( 1, 'buenas')
+
+    }
+
+}
+
+class Page{
+
+    constructor(numeroDeHoja, texto){
+        this.numeroDeHoja = numeroDeHoja;
+        this.texto=texto;
+
+    }
+
+}
+
+let book1 = new Book ('libro1')
